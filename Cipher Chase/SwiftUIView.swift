351cf6f177df81e43c624514fd87147c46a2f4e9
Haifabnf <haifabnf@gmail.com>
2023-11-23 17:00:36 +0300

    Background for main

---
+++ Cipher Chase/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Cipher Chase
+//
+//  Created by haifa alfoiz on 23/11/2023.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
