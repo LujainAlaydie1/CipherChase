8f2bdb77d1f64a2ae7db7d8e47608a4214a0f151
Lujain Alaydie <96392905+LujainAlaydie1@users.noreply.github.com>
2023-11-23 16:35:28 +0300

    added scenarioPage

---
+++ Cipher Chase/scenarioPageTask5.swift
@@ -0,0 +1,18 @@
+//
+//  scenarioPageTask5.swift
+//  Cipher Chase
+//
+//  Created by Lujain Alaydie on 23/11/2023.
+//
+
+import SwiftUI
+
+struct scenarioPageTask5: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    scenarioPageTask5()
+}
