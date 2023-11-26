485aad10ac0f3f26d789f4e4f743416e6462c090
Haifabnf <haifabnf@gmail.com>
2023-11-26 16:53:49 +0300

    Main Page with start game button

---
+++ Cipher Chase/MainPage.swift
@@ -0,0 +1,18 @@
+//
+//  MainPage.swift
+//  Cipher Chase
+//
+//  Created by haifa alfoiz on 26/11/2023.
+//
+
+import SwiftUI
+
+struct MainPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainPage()
+}
