1e25589bb6c59918fc1d1bacbebab1ddd0b63427
Haifabnf <haifabnf@gmail.com>
2023-11-26 17:13:07 +0300

    rest of the buttons but the sizings not right

--- Cipher Chase/MainPage.swift
+++ Cipher Chase/MainPage.swift
@@ -2,14 +2,98 @@
 //  MainPage.swift
 //  Cipher Chase
 //
-//  Created by haifa alfoiz on 26/11/2023.
+//  Created by haifa alfoiz on 23/11/2023.
 //
 
 import SwiftUI
 
 struct MainPage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color(.sRGB, red: 2/255, green: 49/255, blue: 69/255)
+                .edgesIgnoringSafeArea(.all)
+            
+
+            
+            
+            VStack(spacing: 0){ // Title
+                
+                Spacer()
+                
+                Text("Cipher")
+                    .font(Font.custom("PixelifySans-Bold", size: 64))
+                    .shadow(color: Color.black, radius: 4, x: 4, y: 3)
+                    .foregroundStyle(
+                        .linearGradient(
+                            colors: [.accents, .secondarys],
+                            startPoint: .top,
+                            endPoint: .bottom
+                        )
+                    )
+                
+                Text("Chase")
+                    .font(Font.custom("PixelifySans-Bold", size: 64))
+                    .shadow(color: Color.black, radius: 4, x: 4, y: 3)
+                    .foregroundStyle(
+                        .linearGradient(
+                            colors: [.accents, .secondarys],
+                            startPoint: .top,
+                            endPoint: .bottom
+                        )
+                    )
+                
+                Capsule()
+                        .fill(Color.logocolor)
+                        .frame(width: 180 ,height: 24)
+                        .overlay(
+                            Text("Crack the Case")
+                                .font(Font.custom("PixelifySans-Bold", size: 16))
+                                .foregroundStyle(.secondarys)
+                        
+                        )
+                Spacer()
+                
+                VStack{
+                    Button("New Game"){
+                        
+                    }
+                    .buttonStyle(BlueButton())
+                    
+                    Button("Continue"){
+                        
+                    }
+                    .buttonStyle(BlueButton())
+                    
+                    Button("About"){
+                        
+                    }
+                    .buttonStyle(BlueButton())
+
+                    //Spacer()
+                }
+                
+                Image("ones")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 200)
+            }
+        }
+    }
+}
+
+struct BlueButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(Color.logocolor)
+            .frame(width: 190, height: 50)
+            .foregroundStyle(.secondarys)
+            .font(Font.custom("PixelifySans-Bold", size: 32))
+            .cornerRadius(15)
+            //.shadow(color: Color.black, radius: 0.1, x: 0, y: 4)
+            .shadow(color: Color.secondarys, radius: 0, x: 5, y: 7)
+
+
     }
 }
 
