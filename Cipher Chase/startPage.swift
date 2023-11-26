1c0799828cd2a0745f98604e5fc998c21a80edb9
Haifabnf <haifabnf@gmail.com>
2023-11-26 18:03:59 +0300

    the ui is done

---
+++ Cipher Chase/startPage.swift
@@ -0,0 +1,18 @@
+//
+//  startPage.swift
+//  Cipher Chase
+//
+//  Created by haifa alfoiz on 26/11/2023.
+//
+
+import SwiftUI
+
+struct startPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    startPage()
+}
