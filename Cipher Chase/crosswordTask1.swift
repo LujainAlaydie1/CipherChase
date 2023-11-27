b95065aed7831c4d1c30bda5b5eb3d70f034b7f5
Haifabnf <haifabnf@gmail.com>
2023-11-27 17:12:22 +0300

    crossword begining

---
+++ Cipher Chase/crosswordTask1.swift
@@ -0,0 +1,18 @@
+//
+//  crosswordTask1.swift
+//  Cipher Chase
+//
+//  Created by haifa alfoiz on 27/11/2023.
+//
+
+import SwiftUI
+
+struct crosswordTask1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    crosswordTask1()
+}
