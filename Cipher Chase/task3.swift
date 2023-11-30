534d7baf3d7db8e99c0ea1bad3a0dfb4fe53c9a8
iamiralturisi <i.amiralturisi@gmail.com>
2023-11-30 14:49:18 +0300

    senario push

---
+++ Cipher Chase/task3.swift
@@ -0,0 +1,18 @@
+//
+//  task3.swift
+//  Cipher Chase
+//
+//  Created by AMIRA on 16/05/1445 AH.
+//
+
+import SwiftUI
+
+struct task3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    task3()
+}
