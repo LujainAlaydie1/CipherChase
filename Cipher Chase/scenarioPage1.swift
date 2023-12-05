afcb952c383d056bcefb0066a98939ce4c4fde8c
Haifabnf <haifabnf@gmail.com>
2023-12-05 18:35:16 +0300

    completely finished scenarios, only missing thing is navigation between views.

---
+++ Cipher Chase/scenarioPage1.swift
@@ -0,0 +1,18 @@
+//
+//  scenarioPage1.swift
+//  Cipher Chase
+//
+//  Created by haifa alfoiz on 05/12/2023.
+//
+
+import SwiftUI
+
+struct scenarioPage1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    scenarioPage1()
+}
---
+++ Cipher Chase/scenarioPage2.swift
@@ -0,0 +1,18 @@
+//
+//  scenarioPage2.swift
+//  Cipher Chase
+//
+//  Created by haifa alfoiz on 05/12/2023.
+//
+
+import SwiftUI
+
+struct scenarioPage2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    scenarioPage2()
+}
