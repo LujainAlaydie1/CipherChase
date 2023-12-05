d5aac57b413f2d9b918c39793b3be08de878f65a
Haifabnf <haifabnf@gmail.com>
2023-12-05 20:51:29 +0300

    completely done and connected except for main page

---
+++ Cipher Chase/scenarioPagesH.swift
@@ -0,0 +1,18 @@
+//
+//  scenarioPagesH.swift
+//  Cipher Chase
+//
+//  Created by haifa alfoiz on 05/12/2023.
+//
+
+import SwiftUI
+
+struct scenarioPagesH: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    scenarioPagesH()
+}
