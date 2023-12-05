4e609a49960de54b6ef51326c972950e26cd170d
iamiralturisi <i.amiralturisi@gmail.com>
2023-12-05 18:03:09 +0300

    FINISHED TASK3

---
+++ test.swift
@@ -0,0 +1,18 @@
+//
+//  test.swift
+//  Cipher Chase
+//
+//  Created by AMIRA on 20/05/1445 AH.
+//
+
+import SwiftUI
+
+struct test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    test()
+}
